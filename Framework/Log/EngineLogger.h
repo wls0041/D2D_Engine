190f249e8e7295ea768c84d6ce4ef24b60bf05fc
Jin Woo Lee <rladudfk456@naver.com>
2019-03-07 15:55:12 +0900

    190307 GUIDGenerator, IPipeLine, BlendState

---
+++ Framework/Log/EngineLogger.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "Framework.h"
+#include "ILogger.h"
+
+struct LogInfo { std::string text; int type; };
+
+class EngineLogger final : public ILogger
+{
+public:
+	typedef std::function<void(const LogInfo&)> LogFunc;
+
+public:
+	EngineLogger() = default;
+	~EngineLogger() = default;
+
+	EngineLogger(const EngineLogger&) = delete;
+	EngineLogger &operator=(const EngineLogger&) = delete;
+
+	void Log(const std::string &text, const int &type) override {
+		LogInfo logInfo;
+		logInfo.text = text;
+		logInfo.type = type;
+
+		func(logInfo);
+	}
+	void SetCallBack(LogFunc&& func) {
+		this->func = std::forward<LogFunc>(func);
+	}
+
+private:
+	LogFunc func;
+};
